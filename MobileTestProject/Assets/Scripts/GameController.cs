65009683da07b573c36fd8a86bda1937561858c2
zjjc123 <zjjc123@hotmail.com>
2019-03-01 18:40:40 -0800

    Added Basic Get Questions and Change Texts

---
+++ MobileTestProject/Assets/Scripts/GameController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    /* ===========================
+    * Game Controller Class
+    * ===========================
+    * 
+    * Controller class controls how the game is ran in unity and sets everything up
+    * 
+    */
+
+    // Total amount of questions (Change in inspector)
+    [SerializeField]
+    private int totalQuestions = 10;
+
+    [SerializeField]
+    private GameObject questionsText;
+
+    [SerializeField]
+    private GameObject choice1Text;
+    [SerializeField]
+    private GameObject choice2Text;
+    [SerializeField]
+    private GameObject choice3Text;
+    [SerializeField]
+    private GameObject choice4Text;
+
+
+
+    // Start of program
+    void Start()
+    {
+        // Always run the following in order
+
+        // Imports questions to lists of strings from csv
+        QuestionDatabase.ImportGame("Assets/Test.csv");
+        // Set the total amount of questions for the RNG
+        Questions.SetTotalQuestions(totalQuestions);
+        // Load all the strings into lists of Question Set Objects organized by categories
+        Questions.LoadAllQuestions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TO USE ====================
+        // Create new question set and call function Questions.GetQuestion(Category) to get a random question from that category
+        // === QuestionSet q1 = Questions.GetQuestion(QuestionSet.Categories.AboutFBLA);
+
+        // Use getters and setter to access and change the information
+        // === Debug.Log(q1.Answer);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            newQuestion();
+        }
+    }
+
+    private void newQuestion()
+    {
+        QuestionSet q1 = Questions.GetQuestion(QuestionSet.Categories.CompetitveEvents);
+        questionsText.GetComponent<TextMeshProUGUI>().text = q1.Question;
+        choice1Text.GetComponent<TextMeshProUGUI>().text = q1.Choice1;
+        choice2Text.GetComponent<TextMeshProUGUI>().text = q1.Choice2;
+        choice3Text.GetComponent<TextMeshProUGUI>().text = q1.Choice3;
+        choice4Text.GetComponent<TextMeshProUGUI>().text = q1.Choice4;
+
+        Debug.Log("New Questions: " + q1.Question);
+    }
+}
---
+++ MobileTestProject/Assets/Scripts/QuestionDatabase.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+
+/* ===========================
+ * Question Database Class
+ * ===========================
+ * This static class loads all the questions from a csv files stored in format
+ * 
+ * Question, Choice1, Choice2, Choice3, Choice4, Answer, Category
+ * 
+ * Category are in integer format
+ * CompetitveEvents = 1
+ * BusinessSkills = 2
+ * AboutFBLA = 3
+ * FBLAHistory = 4
+ * NationalOfficers = 5
+ * 
+ * IMPORTANT: At the start of program, run import game function with parameter of CSV file path
+ * 
+ */
+public static class QuestionDatabase
+{
+    // Stores all questions, choices, answer, and type of every questions
+    public static List<string> questions;
+    public static List<string> choice1s;
+    public static List<string> choice2s;
+    public static List<string> choice3s;
+    public static List<string> choice4s;
+    public static List<string> answers;
+    public static List<string> categories;
+
+    // Load Questions from a CSV file for easy access
+    public static void ImportGame(string path)
+    {
+        using (var reader = new StreamReader(@path))
+        {
+            
+            questions = new List<string>();
+            choice1s = new List<string>();
+            choice2s = new List<string>();
+            choice3s = new List<string>();
+            choice4s = new List<string>();
+            answers = new List<string>();
+            categories = new List<string>();
+            
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                questions.Add(values[0].ToString());
+                choice1s.Add(values[1].ToString());
+                choice2s.Add(values[2].ToString());
+                choice3s.Add(values[3].ToString());
+                choice4s.Add(values[4].ToString());
+                answers.Add(values[5].ToString());
+                categories.Add(values[6].ToString());
+            }
+        }
+    }
+
+}
